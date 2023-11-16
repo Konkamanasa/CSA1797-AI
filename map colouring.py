9bc1c18375940e7ab036c2fd87c686d95e87f8c5
Konkamanasa <112555886+Konkamanasa@users.noreply.github.com>
2023-11-16 12:39:10 +0530

    Create map colouring.py

---
+++ map colouring.py
@@ -0,0 +1,18 @@
+Welcome to SWI-Prolog (threaded, 64 bits, version 9.0.4)
+SWI-Prolog comes with ABSOLUTELY NO WARRANTY. This is free software.
+Please run ?- license. for legal details.
+
+For online help and background, visit https://www.swi-prolog.org
+For built-in help, use ?- help(Topic). or ?- apropos(Word).
+
+?- 
+% c:/Users/Konka/OneDrive/Documents/Prolog/disease.pl compiled 0.00 sec, 14 clauses
+?- symptom(patient,fever).
+false.
+
+?- hypothesis(amit,disease).
+false.
+
+?- hypothesis(amit,Disease).
+Avoid Oily Food
+Disease = german_measles 
